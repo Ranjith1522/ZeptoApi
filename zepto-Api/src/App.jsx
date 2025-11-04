e0acc7e8f158ef3631b0e6a569906fb1b4024309
ranjith <ranjithkankati2@gmail.com>
2025-11-04 22:38:07 +0530

    arranged the items from get and displayed

--- zepto-Api/src/App.jsx
+++ zepto-Api/src/App.jsx
@@ -2,13 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+
+  const [Product, setProduct] = useState([]);
+  const [AddedItems,AddToCart] = useState([]);
+  const Products = function () {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setProduct(response.data);
+      })
+  }
+
+  const AddedToCart = function () {
+    console.log("Added to Cart");
+  }
+
   
   return (
     <>
+      <h3>Zepto App</h3>
+      <button onClick={Products}>CLick to Fetch</button>
+      <div className='main-container'> 
       
-      <h1>Vite + React</h1>
+       {
+      Product.map((data) => (
+          <div className='card' key={data.id}>
+            <p>{data.id}</p>
+            <p>{data.title}</p>
+            <button>Add to Cart</button>
+          </div>
+        ))
+      }
+      </div>
+
+
     </>
   )
 }
