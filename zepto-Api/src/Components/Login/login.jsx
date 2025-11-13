302db15d2e9f6dc2327a9ede3e5958b5251a34fe
ranjith <ranjithkankati2@gmail.com>
2025-11-13 23:13:40 +0530

    added left and right sides for login

--- zepto-Api/src/Components/Login/login.jsx
+++ zepto-Api/src/Components/Login/login.jsx
@@ -8,8 +8,9 @@ function Login() {
                 <div className="left-side">
                     <img src="https://cdn.zeptonow.com/web-static-assets-prod/artifacts/13.34.0/tr:w-0.2,ar-0.2-0.2,pr-true,f-auto,q-80//images/logo.svg" alt="Zepto Logo" />
                     <h3>Lowest Prices Everydayin 10 minutes*</h3>
-                    <input type="number" placeholder="Enter phone number"></input>
-                    <button type="submit">Continue</button>
+                    <input type="number"className="user-login" placeholder="Enter phone number"></input>
+                    <button className="user-button">Continue</button>
+                    <p>By continuing, you agree to our <a href="#">Terms of Service</a> &Privacy Policy</p>
                 </div>
                 <div className="right-side">
                     <h4 style={{color:'rgb(81,0,128)'}}>Order faster & easier everytime</h4>
