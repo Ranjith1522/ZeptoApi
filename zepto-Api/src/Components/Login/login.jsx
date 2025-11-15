0d94340103b1b013a7ebe47c39773dad69fd5b8b
ranjith <ranjithkankati2@gmail.com>
2025-11-15 08:35:11 +0530

    added login page successfully

--- zepto-Api/src/Components/Login/login.jsx
+++ zepto-Api/src/Components/Login/login.jsx
@@ -5,18 +5,19 @@ function Login() {
     return (
         <>
             <div className="login-container">
-                <div className="input-field">
-                    <img src="https://cdn.zeptonow.com/web-static-assets-prod/artifacts/13.34.0/tr:w-0.2,ar-0.2-0.2,pr-true,f-auto,q-80//images/logo.svg" alt="Zepto Logo" />
-                    <h4>Lowest Prices Everyday in 10 minutes*</h4>
-                    <input type="number" className="username" placeholder="Enter mobile number"/>
-                    <button className="username">Continue</button>
-                </div>
-                <div className="info">
-                    <h4 style={{color:'rgb(81,0,128)'}}>Order faster & easier everytime</h4>
-                    <button><img src="https://cdn.zeptonow.com/web-static-assets-prod/artifacts/13.34.0/tr:w-180,ar-180-46,pr-true,f-auto,q-80//images/app-stores/download-play-store.svg"></img></button>
-                    <button><img src="https://cdn.zeptonow.com/web-static-assets-prod/artifacts/13.34.0/tr:w-180,ar-180-46,pr-true,f-auto,q-80//images/app-stores/download-app-store.svg"></img></button>
+                <div className="login-input">
+                    <img src="https://cdn.zeptonow.com/web-static-assets-prod/artifacts/13.35.1/tr:w-0.2,ar-0.2-0.2,pr-true,f-auto,q-40//images/logo.svg"></img>
+                    <h4>Lowest Prices Everydayin 10 minutes*</h4>
+                    <input type="number" className="input-login" placeholder="Enter mobile number" />
+                    <button className="button-login">Continue</button>
+                    <p>By continuing, you agree to our Terms of Service &Privacy Policy</p>
                 </div>
 
+                <div className="login-content"> 
+                    <h3 className="right-text">Order faster & easier everytime</h3><p> with the Zepto App</p>
+                    <img src="https://cdn.zeptonow.com/web-static-assets-prod/artifacts/13.35.1/tr:w-180,ar-180-46,pr-true,f-auto,q-40//images/app-stores/download-play-store.svg"></img>
+                    <img src="https://cdn.zeptonow.com/web-static-assets-prod/artifacts/13.35.1/tr:w-180,ar-180-46,pr-true,f-auto,q-40//images/app-stores/download-app-store.svg"></img>
+                </div>
             </div>
         </>
     )
